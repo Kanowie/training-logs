c3732b0bb20b3ce0c1d90a86de521ebbdedf5e6d
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-09-15 03:11:27 -0400

    Add app component

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="appContainer">
+        <h1>{{heading}}</h1>
+
+        <Landing /> 
+    </div>
+</template>
+
+<script>
+import Landing from './pages/Landing.vue';
+
+export default {
+    data() {
+        return {
+            heading: 'Vue app container'
+        }
+    },
+    components: {
+        Landing
+    }
+}
+</script>
+
+<style>
+    .appContainer {
+        font-family: 'Roboto', sans-serif;
+        display: flex;
+        justify-content: center;
+    }
+</style>   
