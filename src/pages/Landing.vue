65831bb57f6f50081eac0719eae1d8e69016354a
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-09-15 03:11:43 -0400

    Add landing page

---
+++ src/pages/Landing.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="landingComponent">
+        <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'landing',
+    data() { 
+        return {
+            title: 'Landing Page'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
