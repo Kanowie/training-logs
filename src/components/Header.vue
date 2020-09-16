ccf6411e4d81e48c8bd6e2aa3eda326677138bb2
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-09-16 14:52:19 -0400

    Add Header cpomponent

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="header">
+        <div class="inner-header">
+            <h1>Training-logs</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+.header {
+    width: 100%;
+    height: 130px;
+    display: flex;
+    background-color: black;
+}
+
+.inner-header {
+    width: 80%;
+    height: auto;
+    margin: 30px auto;
+}
+
+.inner-header h1 {
+    font-size: 5rem;
+    color: white;
+    text-align: center;
+    color: aqua;
+}
+</style>
