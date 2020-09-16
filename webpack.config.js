996bbc8beffa3bdeb6dec28e223244ab936eaf88
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-09-16 15:43:50 -0400

    Fix: css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,53 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const webpack = require('webpack');
+const { HotModuleReplacementPlugin } = require('webpack');
+const { join } = require('path');
+
+function resolve(dir) {
+    return join(__dirname, '..', dir);
+}
 
 module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      { test: /\.js$/, use: 'babel-loader' },
-      { test: /\.vue$/, use: 'vue-loader' },
-      { test: /\.css$/, use: ['vue-style-loader', 'css-loader']},
-    ]
-  },
-  devServer: {
-    open: true,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-    new VueLoaderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-  ]
+    entry: './src/index.js',
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            '@': resolve('src'),
+        },
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+        new HotModuleReplacementPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader',
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    devServer: {
+        open: true,
+        hot: true,
+    },
 };
