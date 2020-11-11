27ef48f502a2e1f4dcc816a1a5c72e72cfb939fb
Luis Felipe Hernandez <luis.hernandez093@gmail.com>
2020-11-11 16:04:50 -0500

    Add dynamic list rendering

--- src/pages/Logs.vue
+++ src/pages/Logs.vue
@@ -1,28 +1,14 @@
 <template>
     <div class="container">
-        <div class="logs-container">
-            <h2>Monday-Upper</h2>
+        <div
+            class="logs-container"
+            v-for="trainingSession in trainingSessions"
+            :key="trainingSession.id"
+        >
+            <h2>{{ trainingSession.ssesionType }}</h2>
             <div class="logs-list">
                 <ul>
-                    <li>Push press</li>
-                    <li>Incline Bench</li>
-                    <li>Lat pull downs</li>
-                    <li>Incline rows</li>
-                    <li>Face pulls</li>
-                    <li>Banded push downs</li>
-                </ul>
-            </div>
-        </div>
-        <div class="logs-container">
-            <h2>Tuesday-Lower</h2>
-            <div class="logs-list">
-                <ul>
-                    <li>Push press</li>
-                    <li>Incline Bench</li>
-                    <li>Lat pull downs</li>
-                    <li>Incline rows</li>
-                    <li>Face pulls</li>
-                    <li>Banded push downs</li>
+                    <li v-for="(exercise, index) in trainingSession.sessionExercises" :key="index">{{ exercise  }}</li>
                 </ul>
             </div>
         </div>
@@ -58,9 +44,36 @@
 <script>
 export default {
     name: "logs",
-    data() {
+    data: function () {
         return {
-            title: "Landing Page",
+            trainingSessions: [
+                {
+                    id: 0,
+                    ssesionType: "Monday-Upper",
+                    sessionExercises: [
+                        "Strict Press",
+                        "Incline Bench Press",
+                        "Lat Pull Downs",
+                        "Banded Face Pulls",
+                        "Banded Push Downs",
+                        "One Arm Row",
+                        "3-way Raise",
+                    ],
+                },
+                {
+                    id: 1,
+                    ssesionType: "Tuesday-Upper",
+                    sessionExercises: [
+                        "Squat",
+                        "Squat",
+                        "Squat",
+                        "Squat",
+                        "Squat",
+                        "Squat",
+                        "Squat",
+                    ],
+                },
+            ],
         };
     },
 };
@@ -71,16 +84,14 @@ export default {
     width: 100%;
     height: 1800px;
     display: flex;
-
 }
 
 .logs-container {
     width: 15%;
     height: auto;
     font-size: 1.6rem;
-    margin:  32px auto;
+    margin: 32px auto;
     flex-wrap: wrap;
-
 }
 
 .logs-list {
